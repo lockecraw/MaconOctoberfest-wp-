96c538a797b44d43508144011a1e9230163792a6
Gary Reckard <gary@paleosun.com>
2013-05-13 14:12:26 -0400

    updated sponsors include

--- wp-content/themes/octoberfest-2013/sponsors.php
+++ wp-content/themes/octoberfest-2013/sponsors.php
@@ -8,17 +8,16 @@
   		<a href="http://www.biketechmacon.com/" target="_blank"><img src="<?php bloginfo('template_url'); ?>/images/BikeTechv-140px.png"/></a>
   		<a href="http://www.q106.fm/main.html" target="_blank"><img src="<?php bloginfo('template_url'); ?>/images/1063-140px.jpg"/></a>
 	</div>
-</div>	
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js" type="text/javascript"></script>
+</div>
 <script src="<?php bloginfo('template_url'); ?>/js/jquery-ui-1.8.23.custom.min.js" type="text/javascript"></script>
 <script src="<?php bloginfo('template_url'); ?>/js/jquery.mousewheel.min.js" type="text/javascript"></script>
 <script src="<?php bloginfo('template_url'); ?>/js/jquery.kinetic.js" type="text/javascript"></script>
 <script src="<?php bloginfo('template_url'); ?>/js/jquery.smoothdivscroll-1.3-min.js" type="text/javascript"></script>
 <script type="text/javascript">
 		// Initialize the plugin with no custom options
-		$(document).ready(function () {
+		jQuery(document).ready(function () {
 			// None of the options are set
-			$("div#makeMeScrollable").smoothDivScroll({
+			jQuery("div#makeMeScrollable").smoothDivScroll({
 				autoScrollingMode: "always",
 				mousewheelScrolling: "allDirections",
 				manualContinuousScrolling: true,
