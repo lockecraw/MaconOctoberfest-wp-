f9db2bd716b3fe5fc397002f0c4f875c379a4033
Gary Reckard <greckard@gmail.com>
2014-10-08 12:09:31 -0400

    Backup of Existing Site 2

---
+++ wp-content/uploads/espresso/tickets/templates/indexOctoberfest.php
@@ -0,0 +1,24 @@
+<div class="ticket">
+	<table width="100%" border="0">
+	  <tr>
+		<td width="55%" rowspan="2" valign="top"><span class="top_event_type">Ticket Type: [ticket_type]</span><br>
+		<span class="top_event_title">2014 [event_name]</span><br>
+			Friday - October 24th - 4 p.m. - 10 p.m.<br>
+			Saturday - October 25th - 11 a.m. - 10 p.m.<br>
+			Location: Tattnall Square Park <br> 
+			1155 College St. - Macon, Georgia 31201
+		  <div class="logo"><img src="<?php bloginfo('template_url'); ?>/images/MaconFest.png"/></div></td>
+		<td width="23%" align="right" valign="top"><div class="qr_code">[qr_code]</div></td>
+	  </tr>
+	  <tr>
+		<td colspan="2" align="right" valign="top">
+		<span class="price">Ticket Qty: [ticket_qty]</span>*<br>
+		Price: [cost] x [ticket_qty]<br>
+		  [fname] [lname] (ID: [att_id])<br>
+		  [registration_id]<br/><br/>
+		  </td>
+	  </tr>
+	</table>
+	<br/>
+	*This e-ticket is worth [ticket_qty] [ticket_type] entrie(s) at the gate on a first-come, first-served basis and is only valid for the first [ticket_qty] scans. Please duplicate and distribute with caution. - All ages welcome. Everyone must be 21 years of age or older with valid ID to drink alcohol.
+</div>
